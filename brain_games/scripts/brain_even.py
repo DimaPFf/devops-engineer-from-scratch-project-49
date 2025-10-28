4d1f4b0307845920ffed2204f02530fcd6442609
set1304 <set1304@gmail.com>
2025-10-28 12:32:56 +0300

    fix magic number

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,12 +2,14 @@ import prompt
 import random
 from brain_games.cli import welcome_user
 
+GAME_COUNT = 3
+MIN_COUNT_OF_WIN = 2
 
 def main():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct_answer_count = 0
-    for i in range(3):
+    for i in range(GAME_COUNT):
         num = random.randint(1, 100)
         print(f'Question: {num}')
         answer = prompt.string('Your answer: ')
@@ -20,7 +22,7 @@ def main():
             print('Correct!')
         else:
             print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}".')
-    if correct_answer_count > 2: 
+    if correct_answer_count > MIN_COUNT_OF_WIN: 
         print(f"Congratulations, {name}!")
     else:
         print(f"Let's try again, {name}!")
